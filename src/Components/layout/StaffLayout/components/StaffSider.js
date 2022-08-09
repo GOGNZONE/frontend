f61e1234f2b26bdb9d72189f0ff9a305311ca7e8
mingyu <75217853+gyu0714@users.noreply.github.com>
2022-08-09 10:01:39 +0900

    [UPDATE] icon

--- src/Components/layout/StaffLayout/components/StaffSider.js
+++ src/Components/layout/StaffLayout/components/StaffSider.js
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 import { Layout, Menu } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import {
+  UserOutlined,
+  RocketOutlined,
+  TeamOutlined,
+  DropboxOutlined,
+  FileDoneOutlined,
+  InboxOutlined,
+  PhoneOutlined,
+  CloudServerOutlined,
+} from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 
 const { Sider } = Layout;
@@ -20,28 +29,41 @@ const items = [
   getItem(
     <Link to="/staff/client">거래처 관리</Link>,
     '/client',
-    <UserOutlined />,
+    <TeamOutlined />,
   ),
-  getItem('상품관리', '/production', <UserOutlined />, [
+  getItem('상품 관리', '/production', <DropboxOutlined />, [
     getItem(
       <Link to="/staff/production/list">상품 목록</Link>,
       '/staff/production/list',
     ),
     getItem(<Link to="/staff/production">상품 등록</Link>, '/staff/production'),
   ]),
-  getItem('재고관리', '/stock', <UserOutlined />, [
+  getItem('재고 관리', '/stock', <InboxOutlined />, [
     getItem(<Link to="/staff/stock/list">재고 목록</Link>, '/staff/stock/list'),
     getItem(<Link to="/staff/stock">재고 등록</Link>, '/staff/stock'),
   ]),
-  getItem('출고 관리', '/release', <UserOutlined />, [
+  getItem('출고 관리', '/release', <RocketOutlined />, [
     getItem(
       <Link to="/staff/release/list">출고 목록</Link>,
       '/release/stock/list',
     ),
   ]),
-  getItem('BOM 관리', '/bom', <UserOutlined />, [
-    getItem(<Link to="/staff/bom/list">BOM 목록</Link>, '/release/bom/list'),
-    getItem(<Link to="/staff/bom">BOM 등록</Link>, '/release/bom'),
+  getItem('BOM 관리', '/bom', <FileDoneOutlined />, [
+    getItem(<Link to="/staff/bom/list">BOM 목록</Link>, '/staff/bom/list'),
+    getItem(<Link to="/staff/bom">BOM 등록</Link>, '/staff/bom'),
+  ]),
+  getItem('발주 관리', '/order', <PhoneOutlined />, [
+    getItem(
+      <Link to="/staff/order/list">발주 목록</Link>,
+      '/release/order/list',
+    ),
+  ]),
+  getItem('창고 관리', '/storage', <CloudServerOutlined />, [
+    getItem(
+      <Link to="/staff/storage/list">창고 목록</Link>,
+      '/release/storage/list',
+    ),
+    getItem(<Link to="/staff/storage">창고 등록</Link>, '/release/storage'),
   ]),
 ];
 
