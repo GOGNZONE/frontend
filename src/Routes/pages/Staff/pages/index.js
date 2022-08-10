d62fccc73cd0c32da0454c2fe9469e68caafd100
parkhanju <parkhanju95@gmail.com>
2022-08-10 17:06:55 +0900

    feat: Update Staff index file
    update Staff index file

--- src/Routes/pages/Staff/pages/index.js
+++ src/Routes/pages/Staff/pages/index.js
@@ -1,6 +1,10 @@
 export { default as StaffDashBoard } from './StaffDashBoard';
 export { default as StaffClienList } from './StaffClient';
-export { default as StaffProduction } from './StaffProduction';
+export {
+  StaffProductionList,
+  StaffProductionRegistration,
+  StaffProductionDetails,
+} from './StaffProduction';
 export { default as StaffStock } from './StaffStock';
 export { default as StaffRelease } from './StaffRelease';
 export { default as StaffOrder } from './StaffOrder';
