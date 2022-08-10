6cd27349f9997ed68f9da1acdf770c2e76e15499
parkhanju <parkhanju95@gmail.com>
2022-08-10 23:46:47 +0900

    feat: Update productionApi
    update productionApi

--- src/Apis/api/productionApi.js
+++ src/Apis/api/productionApi.js
@@ -5,3 +5,11 @@ export const getProductionList = async () => {
   const response = await axiosInstance.get('/api/production/list');
   return response;
 };
+
+/* 생산 품목 코드(production_id)로 품목 상세 조회 */
+export const getProduction = async (productionIdParams) => {
+  const response = await axiosInstance.get(
+    `/api/production/${productionIdParams}`,
+  );
+  return response;
+};
