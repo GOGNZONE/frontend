12fbb6d4797068a69efe2f5a67a28596352c8642
parkhanju <parkhanju95@gmail.com>
2022-08-10 17:09:07 +0900

    feat: Update src/index
    commenting React.StrictMode

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import 'antd/dist/antd.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <Router>
+    <App />
+  </Router>,
+  // </React.StrictMode>,
 );
