4dbba073c77e1384c4457c0cea6ed4233d1d8e44
parkhanju <parkhanju95@gmail.com>
2022-08-10 23:44:20 +0900

    feat: Update ProductionDetailsContainer
    update ProductionDetailsContainer

--- src/Routes/pages/Staff/pages/StaffProduction/ProductionDetails/ProductionDetailsContainer.js
+++ src/Routes/pages/Staff/pages/StaffProduction/ProductionDetails/ProductionDetailsContainer.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getProduction } from '../../../../../../Apis/api/productionApi';
 import ProductionDetailsPresenter from './ProductionDetailsPresenter';
 
 const ProductionDetailsContainer = () => {
-  return <ProductionDetailsPresenter />;
+  const { productionIdParams } = useParams();
+  const [componentDisabled, setComponentDisabled] = useState(true);
+  const [production, setProduction] = useState([]);
+
+  useEffect(() => {
+    getProductionApi(productionIdParams);
+  }, [productionIdParams]);
+
+  const onFormLayoutChange = ({ disabled }) => {
+    setComponentDisabled(disabled);
+  };
+
+  const getProductionApi = (productionIdParams) => {
+    getProduction(productionIdParams).then((response) => {
+      setProduction(response.data);
+    });
+  };
+
+  return (
+    <ProductionDetailsPresenter
+      componentDisabled={componentDisabled}
+      setComponentDisabled={setComponentDisabled}
+      onFormLayoutChange={onFormLayoutChange}
+      production={production}
+    />
+  );
 };
 
 export default ProductionDetailsContainer;
