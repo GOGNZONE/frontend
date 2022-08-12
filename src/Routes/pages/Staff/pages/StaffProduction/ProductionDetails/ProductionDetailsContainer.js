043c3281607ba6ef33df585b02316cb0db7345bd
parkhanju <parkhanju95@gmail.com>
2022-08-12 13:54:34 +0900

    feat: Change api folder path
    change api folder path

--- src/Routes/pages/Staff/pages/StaffProduction/ProductionDetails/ProductionDetailsContainer.js
+++ src/Routes/pages/Staff/pages/StaffProduction/ProductionDetails/ProductionDetailsContainer.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getProduction } from '../../../../../../Apis/api/productionApi';
+import { getProduction } from '../../../../../../Apis/productionApi';
 import ProductionDetailsPresenter from './ProductionDetailsPresenter';
 
 const ProductionDetailsContainer = () => {
