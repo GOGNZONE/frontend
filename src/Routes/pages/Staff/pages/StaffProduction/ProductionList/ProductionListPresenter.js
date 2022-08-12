c09aba0819ecae6b5edf46bf4b2cae8fb9763ac7
parkhanju <parkhanju95@gmail.com>
2022-08-12 13:55:54 +0900

    feat: Update ProductionListPresenter
    update ProductionListPresenter

--- src/Routes/pages/Staff/pages/StaffProduction/ProductionList/ProductionListPresenter.js
+++ src/Routes/pages/Staff/pages/StaffProduction/ProductionList/ProductionListPresenter.js
@@ -33,21 +33,21 @@ const columns = [
     title: '비고',
     dataIndex: 'productionDescription',
   },
-  {
-    title: '삭제',
-    dataIndex: 'deleteButton',
-    align: 'center',
-    render: () => (
-      <Button
-        type="primary"
-        size="middle"
-        style={{ backgroundColor: '#D61C4E', border: '#D61C4E' }}
-        onClick={showDeleteConfirm}
-      >
-        삭제
-      </Button>
-    ),
-  },
+  // {
+  //   title: '삭제',
+  //   dataIndex: 'deleteButton',
+  //   align: 'center',
+  //   render: () => (
+  //     <Button
+  //       type="primary"
+  //       size="middle"
+  //       style={{ backgroundColor: '#D61C4E', border: '#D61C4E' }}
+  //       onClick={showDeleteConfirm}
+  //     >
+  //       삭제
+  //     </Button>
+  //   ),
+  // },
 ];
 
 const showDeleteConfirm = () => {
