28d056c2a496c96c1ddfbaed3ec5488cb7bd7c80
parkhanju <parkhanju95@gmail.com>
2022-08-14 10:14:06 +0900

    feat: Update ProductionDetailsContainer
    update ProductionDetailsContainer

--- src/Routes/pages/Staff/pages/StaffProduction/ProductionDetails/ProductionDetailsContainer.js
+++ src/Routes/pages/Staff/pages/StaffProduction/ProductionDetails/ProductionDetailsContainer.js
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getProduction } from '../../../../../../Apis/productionApi';
+import {
+  getProduction,
+  putProduction,
+} from '../../../../../../Apis/productionApi';
 import ProductionDetailsPresenter from './ProductionDetailsPresenter';
+import moment from 'moment';
 
 const ProductionDetailsContainer = () => {
   const { productionIdParams } = useParams();
   const [componentDisabled, setComponentDisabled] = useState(true);
   const [production, setProduction] = useState([]);
+  const [updateButton, setUpdateButton] = useState(true);
 
   useEffect(() => {
     getProductionApi(productionIdParams);
@@ -16,11 +21,43 @@ const ProductionDetailsContainer = () => {
     setComponentDisabled(disabled);
   };
 
+  const onButtonNameChange = () => {
+    setUpdateButton(!updateButton);
+  };
+
   const getProductionApi = (productionIdParams) => {
     getProduction(productionIdParams).then((response) => {
       setProduction(response.data);
     });
   };
+
+  const putProductionApi = (productionIdParams, production) => {
+    putProduction(productionIdParams, production);
+  };
+
+  const onChange = (e) => {
+    let { value, name } = '';
+
+    if (e.target === undefined) {
+      if (moment.isMoment(e.value)) {
+        value = e.value.format('YYYY-MM-DD');
+        name = e.name;
+      } else {
+        value = parseInt(e.value);
+        name = e.name;
+      }
+    } else {
+      value = e.target.value;
+      name = e.target.name;
+    }
+
+    setProduction({
+      ...production,
+      [name]: value,
+    });
+  };
+
+  console.log(production);
 
   return (
     <ProductionDetailsPresenter
@@ -28,6 +65,11 @@ const ProductionDetailsContainer = () => {
       setComponentDisabled={setComponentDisabled}
       onFormLayoutChange={onFormLayoutChange}
       production={production}
+      productionIdParams={productionIdParams}
+      putProductionApi={putProductionApi}
+      onChange={onChange}
+      updateButton={updateButton}
+      onButtonNameChange={onButtonNameChange}
     />
   );
 };
