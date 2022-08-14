2e68fc2d8d04fdfc9dd920ab90850a1d90e2b0fe
parkhanju <parkhanju95@gmail.com>
2022-08-14 10:15:09 +0900

    feat: Update ProductionListPresenter
    update ProductionListPresenter

--- src/Routes/pages/Staff/pages/StaffProduction/ProductionList/ProductionListPresenter.js
+++ src/Routes/pages/Staff/pages/StaffProduction/ProductionList/ProductionListPresenter.js
@@ -1,74 +1,192 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ExclamationCircleOutlined } from '@ant-design/icons';
-import { Button, Table, Typography, Modal, BackTop } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import { Button, Table, Typography, BackTop, Input, Space } from 'antd';
 
-const { confirm } = Modal;
+import Highlighter from 'react-highlight-words';
 
-const columns = [
-  {
-    title: '생산코드',
-    dataIndex: 'productionId',
-  },
-  {
-    title: '생산품목',
-    dataIndex: 'productionName',
-    render: (name, record) => (
-      <Link to={`/staff/production/${record.productionId}`}>{name}</Link>
+// const { confirm } = Modal;
+
+// const showDeleteConfirm = () => {
+//   confirm({
+//     title: '해당 제품을 삭제하시겠습니까?',
+//     icon: <ExclamationCircleOutlined />,
+//     okText: '확인',
+//     okType: 'danger',
+//     cancelText: '취소',
+
+//     onOk() {
+//       console.log('OK');
+//     },
+
+//     onCancel() {
+//       console.log('Cancel');
+//     },
+//   });
+// };
+
+const ProductionListPresenter = ({
+  dataSource,
+  setSearchText,
+  setSearchedColumn,
+  searchInput,
+  searchedColumn,
+  searchText,
+}) => {
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={'생산 코드를 입력해 주세요.'}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            검색
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            초기화
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            검색 취소
+          </Button>
+        </Space>
+      </div>
     ),
-  },
-  {
-    title: '브랜드',
-    dataIndex: 'productionBrandName',
-  },
-  {
-    title: '제품수량',
-    dataIndex: 'productionQuantity',
-  },
-  {
-    title: '단가',
-    dataIndex: 'productionPrice',
-  },
-  {
-    title: '비고',
-    dataIndex: 'productionDescription',
-  },
-  // {
-  //   title: '삭제',
-  //   dataIndex: 'deleteButton',
-  //   align: 'center',
-  //   render: () => (
-  //     <Button
-  //       type="primary"
-  //       size="middle"
-  //       style={{ backgroundColor: '#D61C4E', border: '#D61C4E' }}
-  //       onClick={showDeleteConfirm}
-  //     >
-  //       삭제
-  //     </Button>
-  //   ),
-  // },
-];
-
-const showDeleteConfirm = () => {
-  confirm({
-    title: '해당 제품을 삭제하시겠습니까?',
-    icon: <ExclamationCircleOutlined />,
-    okText: '확인',
-    okType: 'danger',
-    cancelText: '취소',
-
-    onOk() {
-      console.log('OK');
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? '#1890ff' : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
     },
-
-    onCancel() {
-      console.log('Cancel');
-    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#ffc069',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
   });
-};
 
-const ProductionListPresenter = ({ dataSource }) => {
+  const columns = [
+    {
+      title: '생산코드',
+      dataIndex: 'productionId',
+      ...getColumnSearchProps('productionId'),
+    },
+    {
+      title: '생산품목',
+      dataIndex: 'productionName',
+      ...getColumnSearchProps('productionName'),
+      render: (name, record) => (
+        <Link to={`/staff/production/${record.productionId}`}>{name}</Link>
+      ),
+    },
+    {
+      title: '브랜드',
+      dataIndex: 'productionBrandName',
+      ...getColumnSearchProps('productionBrandName'),
+    },
+    {
+      title: '제품수량',
+      dataIndex: 'productionQuantity',
+      defaultSortOrder: 'ascend',
+      sorter: (a, b) => a.productionQuantity - b.productionQuantity,
+    },
+    {
+      title: '단가',
+      dataIndex: 'productionPrice',
+      defaultSortOrder: 'ascend',
+      sorter: (a, b) => a.productionPrice - b.productionPrice,
+    },
+    {
+      title: '비고',
+      dataIndex: 'productionDescription',
+    },
+    // {
+    //   title: '삭제',
+    //   dataIndex: 'deleteButton',
+    //   align: 'center',
+    //   render: () => (
+    //     <Button
+    //       type="primary"
+    //       size="middle"
+    //       style={{ backgroundColor: '#D61C4E', border: '#D61C4E' }}
+    //       onClick={showDeleteConfirm}
+    //     >
+    //       삭제
+    //     </Button>
+    //   ),
+    // },
+  ];
   return (
     <>
       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -102,7 +220,12 @@ const ProductionListPresenter = ({ dataSource }) => {
           </Link>
         </div>
       </div>
-      <Table columns={columns} dataSource={dataSource} />
+      <Table
+        rowKey="productionId"
+        columns={columns}
+        dataSource={dataSource}
+        tableLayout="fixed"
+      />
       <BackTop visibilityHeight={100} />
     </>
   );
