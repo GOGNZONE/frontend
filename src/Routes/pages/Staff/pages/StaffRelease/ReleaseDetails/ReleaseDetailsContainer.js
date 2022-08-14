60697214bafd6f6608a8a6a76aceaf20e9de5e52
parkhanju <parkhanju95@gmail.com>
2022-08-14 10:18:03 +0900

    feat: Create ReleaseDetailsContainer
    create ReleaseDetailsContainer

---
+++ src/Routes/pages/Staff/pages/StaffRelease/ReleaseDetails/ReleaseDetailsContainer.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getRelease } from '../../../../../../Apis/releaseApi';
+import ReleaseDetailsPresenter from './ReleaseDetailsPresenter';
+
+const ReleaseDetailsContainer = () => {
+  const { releaseIdParams } = useParams();
+  const [release, setRelease] = useState({
+    releaseDate: '',
+    releaseDescription: '',
+    releaseQuantity: '',
+    releaseTotalPrice: '',
+    releaseType: '',
+    releaseClientDto: {
+      clientId: '',
+      clientName: '',
+      clientManager: '',
+      clientTel: '',
+      clientAddress: '',
+      employeeName: '',
+    },
+    releaseProductionDto: {
+      productionName: '',
+      productionBrandName: '',
+      productionPrice: '',
+    },
+    deliveryDto: {
+      deliveryId: '',
+      deliveryCompanyName: '',
+      deliveryTrackingNumber: '',
+    },
+  });
+
+  useEffect(() => {
+    getReleaseApi(releaseIdParams);
+  }, [releaseIdParams]);
+
+  const getReleaseApi = (releaseIdParams) => {
+    getRelease(releaseIdParams).then((response) => {
+      setRelease(response.data);
+    });
+  };
+
+  return <ReleaseDetailsPresenter release={release} />;
+};
+
+export default ReleaseDetailsContainer;
