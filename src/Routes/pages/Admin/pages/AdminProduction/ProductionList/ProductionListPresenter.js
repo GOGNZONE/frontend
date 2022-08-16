85c3b7f04e8852ddee1db2e2c8c93601dcdd6289
parkhanju <parkhanju95@gmail.com>
2022-08-16 15:50:51 +0900

    feat: Create Production list presenter
    create Production list presenter

---
+++ src/Routes/pages/Admin/pages/AdminProduction/ProductionList/ProductionListPresenter.js
@@ -0,0 +1,250 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { SearchOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
+import { Button, Table, Typography, BackTop, Input, Space, Modal } from 'antd';
+
+import Highlighter from 'react-highlight-words';
+
+const { confirm } = Modal;
+
+const showDeleteConfirm = () => {
+  confirm({
+    title: '해당 제품을 삭제하시겠습니까?',
+    icon: <ExclamationCircleOutlined />,
+    okText: '확인',
+    okType: 'danger',
+    cancelText: '취소',
+
+    onOk() {
+      console.log('OK');
+    },
+
+    onCancel() {
+      console.log('Cancel');
+    },
+  });
+};
+
+const ProductionListPresenter = ({
+  dataSource,
+  setSearchText,
+  setSearchedColumn,
+  searchInput,
+  searchedColumn,
+  searchText,
+}) => {
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={'생산 코드를 입력해 주세요.'}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            검색
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            초기화
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            검색 취소
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? '#1890ff' : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#ffc069',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const columns = [
+    {
+      title: '생산코드',
+      dataIndex: 'productionId',
+      ...getColumnSearchProps('productionId'),
+    },
+    {
+      title: '생산품목',
+      dataIndex: 'productionName',
+      ...getColumnSearchProps('productionName'),
+      render: (name, record) => (
+        <Link to={`/admin/production/${record.productionId}`}>{name}</Link>
+      ),
+    },
+    {
+      title: '브랜드',
+      dataIndex: 'productionBrandName',
+      ...getColumnSearchProps('productionBrandName'),
+    },
+    {
+      title: '제품수량',
+      dataIndex: 'productionQuantity',
+      defaultSortOrder: 'ascend',
+      sorter: (a, b) => a.productionQuantity - b.productionQuantity,
+    },
+    {
+      title: '단가',
+      dataIndex: 'productionPrice',
+      defaultSortOrder: 'ascend',
+      sorter: (a, b) => a.productionPrice - b.productionPrice,
+    },
+    {
+      title: '비고',
+      dataIndex: 'productionDescription',
+    },
+    {
+      title: '출고',
+      dataIndex: 'releaseButton',
+      align: 'center',
+      render: () => (
+        <Link to="/admin/release">
+          <Button
+            type="primary"
+            size="middle"
+            style={{ backgroundColor: '#293462', border: '#293462' }}
+          >
+            출고
+          </Button>
+        </Link>
+      ),
+    },
+    {
+      title: '삭제',
+      dataIndex: 'deleteButton',
+      align: 'center',
+      render: () => (
+        <Button
+          type="primary"
+          size="middle"
+          style={{ backgroundColor: '#D61C4E', border: '#D61C4E' }}
+          onClick={showDeleteConfirm}
+        >
+          삭제
+        </Button>
+      ),
+    },
+  ];
+  return (
+    <>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography.Title level={3} style={{ margin: 5 }}>
+          생산 목록
+        </Typography.Title>
+        <div>
+          <Link to="/admin/release/list">
+            <Button
+              type="primary"
+              style={{
+                margin: 5,
+                backgroundColor: '#293462',
+                border: '#293462',
+              }}
+            >
+              출고 목록
+            </Button>
+          </Link>
+          <Link to="/admin/production">
+            <Button
+              type="primary"
+              style={{
+                margin: 5,
+                backgroundColor: '#FEB139',
+                border: '#FEB139',
+              }}
+            >
+              등록
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <Table
+        rowKey="productionId"
+        columns={columns}
+        dataSource={dataSource}
+        tableLayout="fixed"
+      />
+      <BackTop visibilityHeight={100} />
+    </>
+  );
+};
+
+export default ProductionListPresenter;
