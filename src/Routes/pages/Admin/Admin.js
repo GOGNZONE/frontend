2c0e12a7db979d0282702bd510baa645f7ced158
parkhanju <parkhanju95@gmail.com>
2022-08-16 15:54:24 +0900

    feat: Add Production and Release routes
    add Production and Release routes

--- src/Routes/pages/Admin/Admin.js
+++ src/Routes/pages/Admin/Admin.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { AdminLayout } from '../../../Components';
+import {
+  AdminProductionDetails,
+  AdminProductionList,
+  AdminProductionRegistration,
+  AdminReleaseList,
+  AdminReleaseDetails,
+  AdminReleaseRegistration,
+} from './pages';
 
 const Admin = () => {
   return (
     <Routes>
-      <Route path="/" element={<AdminLayout />}></Route>
+      <Route path="/" element={<AdminLayout />}>
+        {/* production */}
+        <Route path="/production" element={<AdminProductionRegistration />} />
+        <Route path="/production/list" element={<AdminProductionList />} />
+        <Route
+          path="/production/:productionIdParams"
+          element={<AdminProductionDetails />}
+        />
+        {/* release */}
+        <Route
+          path="/release/:productionIdParams"
+          element={<AdminReleaseRegistration />}
+        />
+        <Route path="/release/list" element={<AdminReleaseList />} />
+        <Route
+          path="/release/:releaseIdParams"
+          element={<AdminReleaseDetails />}
+        />
+      </Route>
     </Routes>
   );
 };
