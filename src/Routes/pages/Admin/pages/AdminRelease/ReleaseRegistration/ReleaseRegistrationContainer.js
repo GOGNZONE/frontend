f48c9b92649d8eaf97c7b677232f7b3477a55e9e
parkhanju <parkhanju95@gmail.com>
2022-08-16 15:44:54 +0900

    feat: Create Release registration container
    create Release registration container

---
+++ src/Routes/pages/Admin/pages/AdminRelease/ReleaseRegistration/ReleaseRegistrationContainer.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getRelease } from '../../../../../../Apis/releaseApi';
+import ReleaseRegistrationPresenter from './ReleaseRegistrationPresenter';
+
+const ReleaseRegistrationContainer = () => {
+  const { releaseIdParams } = useParams();
+  const [release, setRelease] = useState({
+    releaseDate: '',
+    releaseDescription: '',
+    releaseQuantity: '',
+    releaseTotalPrice: '',
+    releaseType: '',
+    releaseClientDto: {
+      clientId: '',
+      clientName: '',
+      clientManager: '',
+      clientTel: '',
+      clientAddress: '',
+      employeeName: '',
+    },
+    releaseProductionDto: {
+      productionName: '',
+      productionBrandName: '',
+      productionPrice: '',
+    },
+    deliveryDto: {
+      deliveryId: '',
+      deliveryCompanyName: '',
+      deliveryTrackingNumber: '',
+    },
+  });
+
+  useEffect(() => {
+    getReleaseApi(releaseIdParams);
+  }, [releaseIdParams]);
+
+  const getReleaseApi = (releaseIdParams) => {
+    getRelease(releaseIdParams).then((response) => {
+      setRelease(response.data);
+    });
+  };
+
+  return <ReleaseRegistrationPresenter release={release} />;
+};
+
+export default ReleaseRegistrationContainer;
