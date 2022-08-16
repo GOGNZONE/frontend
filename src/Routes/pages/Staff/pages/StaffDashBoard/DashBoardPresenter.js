43ab1b572a52aba29aeecd492ddcdec580663f24
parkhanju <parkhanju95@gmail.com>
2022-08-16 10:27:01 +0900

    feat: Update DashBoardPresenter
    update DashBoardPresenter

--- src/Routes/pages/Staff/pages/StaffDashBoard/DashBoardPresenter.js
+++ src/Routes/pages/Staff/pages/StaffDashBoard/DashBoardPresenter.js
@@ -1,28 +1,6 @@
 import React from 'react';
 import { UserOutlined } from '@ant-design/icons';
-import {
-  PageHeader,
-  Row,
-  Tag,
-  Avatar,
-  Form,
-  Input,
-  InputNumber,
-  Space,
-} from 'antd';
-
-const Content = ({ children, extraContent }) => (
-  <Row>
-    <div
-      style={{
-        flex: 1,
-      }}
-    >
-      {children}
-    </div>
-    <div className="image">{extraContent}</div>
-  </Row>
-);
+import { PageHeader, Tag, Avatar, Form, Input } from 'antd';
 
 const DashBoardPresenter = () => (
   <PageHeader
@@ -38,31 +16,32 @@ const DashBoardPresenter = () => (
       <Avatar size={120} icon={<UserOutlined />} />
       <Form
         labelCol={{
-          span: 7,
+          span: 5,
         }}
         wrapperCol={{
-          span: 10,
+          span: 18,
         }}
         layout="horizontal"
         size="large"
+        style={{ width: '500px' }}
       >
         <Form.Item label="사번">
           <Input disabled={true} />
         </Form.Item>
         <Form.Item label="이름">
-          <Input name="productionName" placeholder="생산 제품명" />
+          <Input name="productionName" />
         </Form.Item>
         <Form.Item label="주소">
-          <Input name="productionBrandName" placeholder="생산 제품 브랜드명" />
+          <Input name="productionBrandName" />
         </Form.Item>
         <Form.Item label="이메일">
-          <Input name="productionBrandName" placeholder="생산 제품 브랜드명" />
+          <Input name="productionBrandName" />
         </Form.Item>
         <Form.Item label="연락처">
-          <Input name="productionBrandName" placeholder="생산 제품 브랜드명" />
+          <Input name="productionBrandName" />
         </Form.Item>
         <Form.Item label="입사일">
-          <Input name="productionStandard" placeholder="생산 제품 규격" />
+          <Input name="productionStandard" disabled={true} />
         </Form.Item>
       </Form>
     </div>
