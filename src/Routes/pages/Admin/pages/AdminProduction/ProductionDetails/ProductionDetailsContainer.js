2c51c4944f5549bc07baf5745e76806caf3c403b
parkhanju <parkhanju95@gmail.com>
2022-08-16 15:52:12 +0900

    feat: Create Production details container
    create Production details container

---
+++ src/Routes/pages/Admin/pages/AdminProduction/ProductionDetails/ProductionDetailsContainer.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import {
+  getProduction,
+  putProduction,
+} from '../../../../../../Apis/productionApi';
+import ProductionDetailsPresenter from './ProductionDetailsPresenter';
+import moment from 'moment';
+
+const ProductionDetailsContainer = () => {
+  const { productionIdParams } = useParams();
+  const [componentDisabled, setComponentDisabled] = useState(true);
+  const [production, setProduction] = useState([]);
+  const [updateButton, setUpdateButton] = useState(true);
+
+  useEffect(() => {
+    getProductionApi(productionIdParams);
+  }, [productionIdParams]);
+
+  const onFormLayoutChange = ({ disabled }) => {
+    setComponentDisabled(disabled);
+  };
+
+  const onButtonNameChange = () => {
+    setUpdateButton(!updateButton);
+  };
+
+  const getProductionApi = (productionIdParams) => {
+    getProduction(productionIdParams).then((response) => {
+      setProduction(response.data);
+    });
+  };
+
+  const putProductionApi = (productionIdParams, production) => {
+    putProduction(productionIdParams, production);
+  };
+
+  const onChange = (e) => {
+    let { value, name } = '';
+
+    if (e.target === undefined) {
+      if (moment.isMoment(e.value)) {
+        value = e.value.format('YYYY-MM-DD');
+        name = e.name;
+      } else {
+        value = parseInt(e.value);
+        name = e.name;
+      }
+    } else {
+      value = e.target.value;
+      name = e.target.name;
+    }
+
+    setProduction({
+      ...production,
+      [name]: value,
+    });
+  };
+
+  console.log(production);
+
+  return (
+    <ProductionDetailsPresenter
+      componentDisabled={componentDisabled}
+      setComponentDisabled={setComponentDisabled}
+      onFormLayoutChange={onFormLayoutChange}
+      production={production}
+      productionIdParams={productionIdParams}
+      putProductionApi={putProductionApi}
+      onChange={onChange}
+      updateButton={updateButton}
+      onButtonNameChange={onButtonNameChange}
+    />
+  );
+};
+
+export default ProductionDetailsContainer;
