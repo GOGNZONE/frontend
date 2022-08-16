b8865724e8f444b30c362df517b41aa594d71d5e
parkhanju <parkhanju95@gmail.com>
2022-08-16 15:43:52 +0900

    feat: Create index for named export
    create index for named export

---
+++ src/Routes/pages/Admin/pages/index.js
@@ -0,0 +1,11 @@
+export {
+  AdminProductionList,
+  AdminProductionDetails,
+  AdminProductionRegistration,
+} from './AdminProduction';
+
+export {
+  AdminReleaseList,
+  AdminReleaseDetails,
+  AdminReleaseRegistration,
+} from './AdminRelease';
