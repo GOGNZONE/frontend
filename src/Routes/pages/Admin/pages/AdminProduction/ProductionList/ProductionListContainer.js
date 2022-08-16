d3bdcdcc691789c2469565778ea21acbff90b5f9
parkhanju <parkhanju95@gmail.com>
2022-08-16 15:51:10 +0900

    feat: Create Production list container
    create Production list container

---
+++ src/Routes/pages/Admin/pages/AdminProduction/ProductionList/ProductionListContainer.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState, useRef } from 'react';
+import ProductionListPresenter from './ProductionListPresenter';
+import { getProductionList } from '../../../../../../Apis/productionApi';
+
+const ProductionListContainer = () => {
+  const [productions, setProductions] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
+
+  useEffect(() => {
+    getProductionListApi();
+  }, []);
+
+  const getProductionListApi = () => {
+    getProductionList().then((response) => {
+      setProductions(response.data);
+    });
+  };
+
+  return (
+    <ProductionListPresenter
+      dataSource={productions}
+      setSearchText={setSearchText}
+      setSearchedColumn={setSearchedColumn}
+      searchInput={searchInput}
+      searchedColumn={searchedColumn}
+      searchText={searchText}
+    />
+  );
+};
+
+export default ProductionListContainer;
