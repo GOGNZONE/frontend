66f0b375fbb7f169b3d384d14ab0f8310efe8c07
parkhanju <parkhanju95@gmail.com>
2022-08-19 11:33:33 +0900

    feat: Add react-redux store
    add react-redux store

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import { App } from './App';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'antd/dist/antd.min.css';
+import { Provider } from 'react-redux';
+
+import store from './store/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <Router>
-    <App />
-  </Router>,
-  // </React.StrictMode>,
+  <Provider store={store()}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  // </React.StrictMode>
 );
