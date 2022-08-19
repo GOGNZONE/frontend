93c59a4d25901487283a252f3872d8522fdd51dd
parkhanju <parkhanju95@gmail.com>
2022-08-19 11:39:00 +0900

    feat: Create configureStore
    create configureStore

---
+++ src/store/configureStore.js
@@ -0,0 +1,15 @@
+import { applyMiddleware, createStore, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducer from '../modules';
+
+const configureStore = () => {
+  const middlewares = [];
+  const enhancer =
+    process.env.NODE_ENV === 'production'
+      ? compose(applyMiddleware(...middlewares))
+      : composeWithDevTools(applyMiddleware(...middlewares));
+  const store = createStore(reducer, enhancer);
+  return store;
+};
+
+export default configureStore;
