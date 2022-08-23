571eaa18b3a4e149f7fc6b40e380adeefc364730
mingyu <75217853+gyu0714@users.noreply.github.com>
2022-08-23 18:54:07 +0900

    [UPDATE] mypage ui

--- src/Routes/pages/Staff/pages/StaffMypage/StaffMypageContainer.js
+++ src/Routes/pages/Staff/pages/StaffMypage/StaffMypageContainer.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import StaffMypagePresenter from './StaffMypagePresenter';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMypage } from 'modules/mypage';
 
 const StaffMypageContainer = () => {
-  return <StaffMypagePresenter />;
+  const { data, loading, error } = useSelector((state) => state.mypage.mypage);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMypage());
+  }, [dispatch]);
+
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러 발생!</div>;
+  if (!data) return null;
+  return <StaffMypagePresenter mypage={data} />;
 };
 
 export default StaffMypageContainer;
--- src/Routes/pages/Staff/pages/StaffMypage/StaffMypagePresenter.js
+++ src/Routes/pages/Staff/pages/StaffMypage/StaffMypagePresenter.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Image, Descriptions, Col, Row } from 'antd';
+import Profile from 'assets/test.png';
 
-const StaffMypagePresenter = () => {
-  return <div>StaffMypagePresenter</div>;
+const StaffMypagePresenter = ({ mypage }) => {
+  return (
+    <>
+      <Row align="middle" gutter={8}>
+        <Col>
+          <Image width={250} height={250} src={Profile} />
+        </Col>
+        <Col flex={4}>
+          <Descriptions title="마이페이지" bordered>
+            <Descriptions.Item label="사번">
+              {mypage.employeeId}
+            </Descriptions.Item>
+            <Descriptions.Item label="이름" span={2}>
+              {mypage.employeeName}
+            </Descriptions.Item>
+            <Descriptions.Item label="연락처">
+              {mypage.employeePhone}
+            </Descriptions.Item>
+            <Descriptions.Item label="입사일자" span={2}>
+              {mypage.employeeHiredate}
+            </Descriptions.Item>
+            <Descriptions.Item label="주소" span={3}>
+              {mypage.employeeAddress}
+            </Descriptions.Item>
+            <Descriptions.Item label="이메일">
+              {mypage.employeeEmail}
+            </Descriptions.Item>
+          </Descriptions>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
-export default StaffMypagePresenter;
+export default React.memo(StaffMypagePresenter);
