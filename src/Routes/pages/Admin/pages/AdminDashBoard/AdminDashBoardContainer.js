c60b19ee701d46c75ff2bd2fc6d433b9c6b2b498
mingyu <75217853+gyu0714@users.noreply.github.com>
2022-08-23 15:06:15 +0900

    [UPDATE] admin dashboard route

---
+++ src/Routes/pages/Admin/pages/AdminDashBoard/AdminDashBoardContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AdminDashBoardPresenter from './AdminDashBoardPresenter';
+
+const AdminDashBoardContainer = () => {
+  return <AdminDashBoardPresenter />;
+};
+
+export default AdminDashBoardContainer;
