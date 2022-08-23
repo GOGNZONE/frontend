fd8c35c1de875cdf4f42029371eeeeba63fbd1db
mingyu <75217853+gyu0714@users.noreply.github.com>
2022-08-23 18:56:13 +0900

    initial commit

--- src/Components/layout/AdminLayout/components/AdminHeader.js
+++ src/Components/layout/AdminLayout/components/AdminHeader.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { UserOutlined } from '@ant-design/icons';
 import { Layout, Avatar } from 'antd';
-import { useNavigate } from 'react-router-dom';
 
 const { Header } = Layout;
 
