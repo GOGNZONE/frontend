68628652bf87e041c937fe0c2d400439a114d840
mingyu <75217853+gyu0714@users.noreply.github.com>
2022-08-23 18:55:15 +0900

    [UPDATE] header layout

--- src/Components/layout/AdminLayout/components/AdminHeader.js
+++ src/Components/layout/AdminLayout/components/AdminHeader.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import { UserOutlined } from '@ant-design/icons';
 import { Layout, Avatar } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 const { Header } = Layout;
 
 const AdminHeader = ({ logout }) => {
   return (
     <Header className="site-layout-background" style={{ padding: '0px 30px' }}>
-      <div style={{ float: 'left' }}>
-        <h1>{/* {pageTitle} */}Title</h1>
-      </div>
       <div style={{ float: 'right' }}>
         <>
           <Avatar icon={<UserOutlined />} style={{ marginRight: '10px' }} />
-          <span style={{ marginRight: '20px' }}>
-            {/* {manager_nm} */}
-            STAFF
-          </span>
+          <span style={{ marginRight: '20px' }}>ADMIN</span>
           <span className="site-header-logout" onClick={logout}>
             로그아웃
           </span>
--- src/Components/layout/StaffLayout/components/StaffHeader.js
+++ src/Components/layout/StaffLayout/components/StaffHeader.js
@@ -7,16 +7,10 @@ const { Header } = Layout;
 const StaffHeader = ({ logout }) => {
   return (
     <Header className="site-layout-background" style={{ padding: '0px 30px' }}>
-      <div style={{ float: 'left' }}>
-        <h1>{/* {pageTitle} */}Title</h1>
-      </div>
       <div style={{ float: 'right' }}>
         <>
           <Avatar icon={<UserOutlined />} style={{ marginRight: '10px' }} />
-          <span style={{ marginRight: '20px' }}>
-            {/* {manager_nm} */}
-            STAFF
-          </span>
+          <span style={{ marginRight: '20px' }}>STAFF</span>
           <span className="site-header-logout" onClick={logout}>
             로그아웃
           </span>
