8c495bfbda323308269f92be76f4bb5703e4a62b
parkhanju <parkhanju95@gmail.com>
2022-08-26 12:19:53 +0900

    feat: Update index.js
    update index.js

--- src/index.js
+++ src/index.js
@@ -6,23 +6,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import 'antd/dist/antd.min.css';
 import { Provider } from 'react-redux';
 
-import store from 'store/configureStore';
-// import rootReducer from './modules';
-// import { createStore } from 'redux';
+import store from './store/configureStore';
 
-/** redux */
-// import rootReducer from './modules';
-// import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-// import ReduxThunk from 'redux-thunk';
-
-// const store = createStore(
-//   rootReducer,
-//   composeWithDevTools(applyMiddleware(ReduxThunk)),
-// );
-
-// const store = createStore(rootReducer);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store()}>
