6e6b12c317e52bb7caf337d98da6b122820f7750
parkhanju <parkhanju95@gmail.com>
2022-08-29 14:50:40 +0900

    feat: Add ReleaseRegistrationInProduction export code
    add ReleaseRegistrationInProduction export code

--- src/Routes/pages/Admin/pages/AdminRelease/index.js
+++ src/Routes/pages/Admin/pages/AdminRelease/index.js
@@ -1,3 +1,4 @@
 export { default as AdminReleaseList } from './ReleaseList';
-export { default as AdminReleaseDetails } from './ReleaseDetails';
 export { default as AdminReleaseRegistration } from './ReleaseRegistration';
+export { default as AdminReleaseRegistrationInProduction } from './ReleaseRegistrationInProduction';
+export { default as AdminReleaseDetails } from './ReleaseDetails';
