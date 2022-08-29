51ba8d2fdbf8de145db14b41d4df62e20ba098f3
mingyu <75217853+gyu0714@users.noreply.github.com>
2022-08-29 17:50:41 +0900

    [DONE] admin employee detail page

--- src/Routes/pages/Admin/Admin.js
+++ src/Routes/pages/Admin/Admin.js
@@ -4,6 +4,7 @@ import { AdminLayout } from 'Components';
 import {
   AdminClientList,
   AdminDashBoard,
+  AdminEmployeeDetails,
   AdminEmployeeList,
   AdminRegisterEmployee,
   AdminRetiredEmployeeList,
@@ -37,24 +38,34 @@ const Admin = ({ checkAdmin, logout }) => {
         element={<AdminLayout checkAdmin={checkAdmin} logout={logout} />}
       >
         <Route index element={<AdminDashBoard />} />
+        {/* Employee */}
         <Route path="/employee/list" element={<AdminEmployeeList />} />
+        <Route
+          path="/employee/:employeeId"
+          element={<AdminEmployeeDetails />}
+        />
         <Route path="/employee" element={<AdminRegisterEmployee />} />
         <Route
           path="/retired-employee/list"
           element={<AdminRetiredEmployeeList />}
         />
+        {/* Client */}
         <Route path="/client/list" element={<AdminClientList />} />
+        {/* Stock */}
         <Route path="/stock/list" element={<AdminStockList />} />
         <Route path="/stock/list/:stockIdParams" element={<AdminStockInfo />} />
         <Route path="/stock" element={<AdminStockRegist />} />
+        {/* Order */}
         <Route path="/order" element={<AdminOrderRegist />} />
         <Route path="/order/list" element={<AdminOrderList />} />
         <Route path="/order/list/:orderIdParams" element={<AdminOrderInfo />} />
         <Route path="/order/info" element={<AdminOrderInfo />} />
+        {/* BOM */}
         <Route path="/bom" element={<AdminBomRegist />} />
         <Route path="/bom/list" element={<AdminBomList />} />
         <Route path="/bom/list/:bomIdParams" element={<AdminBomInfo />} />
         <Route path="/bom" element={<AdminBomRegist />} />
+        {/* Storage */}
         <Route path="/storage/list" element={<AdminStorageList />} />
         <Route
           path="/storage/list/:storageIdParams"
