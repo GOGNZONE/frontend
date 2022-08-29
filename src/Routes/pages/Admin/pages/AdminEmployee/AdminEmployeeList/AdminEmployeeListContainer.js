9dd9dd1c08439760434728f7b5317f5ae63904c4
mingyu <75217853+gyu0714@users.noreply.github.com>
2022-08-29 15:07:44 +0900

    [UPDATE] employee list done

--- src/Routes/pages/Admin/pages/AdminEmployee/AdminEmployeeList/AdminEmployeeListContainer.js
+++ src/Routes/pages/Admin/pages/AdminEmployee/AdminEmployeeList/AdminEmployeeListContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   deleteEmployee,
@@ -7,13 +7,16 @@ import {
 import AdminEmployeeListPresenter from './AdminEmployeeListPresenter';
 
 const AdminEmployeeListContainer = () => {
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
   const { data, loading, error } = useSelector(
     (state) => state.employee.employeeList,
   );
   const dispatch = useDispatch();
 
-  const onDeleteHandler = (employeeId) => {
-    dispatch(deleteEmployee(employeeId));
+  const onDeleteHandler = async (employeeId) => {
+    await dispatch(deleteEmployee(employeeId));
     dispatch(getEmployeeList());
   };
 
@@ -27,6 +30,11 @@ const AdminEmployeeListContainer = () => {
       loading={loading}
       error={error}
       onDeleteHandler={onDeleteHandler}
+      setSearchText={setSearchText}
+      setSearchedColumn={setSearchedColumn}
+      searchInput={searchInput}
+      searchedColumn={searchedColumn}
+      searchText={searchText}
     />
   );
 };
--- src/Routes/pages/Admin/pages/AdminEmployee/AdminEmployeeList/AdminEmployeeListPresenter.js
+++ src/Routes/pages/Admin/pages/AdminEmployee/AdminEmployeeList/AdminEmployeeListPresenter.js
@@ -1,14 +1,170 @@
-import { Spin } from 'antd';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
+import { SearchOutlined } from '@ant-design/icons';
+import { Button, Table, Typography, BackTop, Input, Space, Spin } from 'antd';
+import Highlighter from 'react-highlight-words';
 
 const AdminEmployeeListPresenter = ({
   employeeList,
   loading,
   error,
   onDeleteHandler,
+  setSearchText,
+  setSearchedColumn,
+  searchInput,
+  searchedColumn,
+  searchText,
 }) => {
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={'생산 코드를 입력해 주세요.'}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            검색
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            초기화
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            검색 취소
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? '#1890ff' : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#ffc069',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const columns = [
+    {
+      title: '사원번호',
+      dataIndex: 'employeeId',
+      width: 180,
+      ...getColumnSearchProps('employeeId'),
+      sorter: (a, b) => a.employeeId - b.employeeId,
+    },
+    {
+      title: '이름',
+      dataIndex: 'employeeName',
+      width: 200,
+      ...getColumnSearchProps('employeeName'),
+      render: (name, record) => (
+        <Link to={`/staff/employee/${record.employeeId}`}>{name}</Link>
+      ),
+    },
+    {
+      title: '주소',
+      dataIndex: 'employeeAddress',
+      width: 200,
+    },
+    {
+      title: '전화번호',
+      dataIndex: 'employeePhone',
+      defaultSortOrder: 'ascend',
+      width: 200,
+    },
+    {
+      title: '입사일',
+      dataIndex: 'employeeHiredate',
+      defaultSortOrder: 'ascend',
+      width: 200,
+    },
+    {
+      title: '권한',
+      dataIndex: 'employeeRole',
+    },
+    {
+      title: '비고',
+      render: (record) => (
+        <Button onClick={() => onDeleteHandler(record.employeeId)}>삭제</Button>
+      ),
+    },
+  ];
+
   if (loading) return <Spin spinning={loading} size="large" />;
   if (error)
     return Swal.fire({
@@ -21,21 +177,33 @@ const AdminEmployeeListPresenter = ({
   if (!employeeList) return null;
   return (
     <>
-      <ul>
-        {employeeList.map((employee) => (
-          <li key={employee.employeeId}>
-            <Link to={`/admin/employee/${employee.employeeId}`}>
-              {employee.employeeName}
-            </Link>
-            <button
-              type="button"
-              onClick={() => onDeleteHandler(employee.employeeId)}
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography.Title level={3} style={{ margin: 5 }}>
+          사원 목록
+        </Typography.Title>
+        <div>
+          <Link to="/admin/employee">
+            <Button
+              type="primary"
+              style={{
+                margin: 5,
+                backgroundColor: '#FEB139',
+                border: '#FEB139',
+              }}
             >
-              삭제
-            </button>
-          </li>
-        ))}
-      </ul>
+              등록
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <Spin spinning={loading}>
+        <Table
+          rowKey="employeeId"
+          columns={columns}
+          dataSource={employeeList}
+        />
+      </Spin>
+      <BackTop visibilityHeight={100} />
     </>
   );
 };
