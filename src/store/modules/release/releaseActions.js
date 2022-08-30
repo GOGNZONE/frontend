dc78a5d562a79168cee620d27de6c39ddc9b2c5a
parkhanju <parkhanju95@gmail.com>
2022-08-30 23:37:27 +0900

    feat: Update releaseActions
    update releaseActions

--- src/store/modules/release/releaseActions.js
+++ src/store/modules/release/releaseActions.js
@@ -1,4 +1,4 @@
-import * as api from 'apis/index';
+import * as api from 'apis';
 import { createPromiseThunk } from 'lib/asyncUtils';
 
 export const GET_RELEASES = 'GET_RELEASES';
@@ -17,6 +17,12 @@ export const PUT_RELEASE = 'PUT_RELEASE';
 export const PUT_RELEASE_SUCCESS = 'PUT_RELEASE_SUCCESS';
 export const PUT_RELEASE_ERROR = 'PUT_RELEASE_ERROR';
 
+export const DELETE_RELEASE = 'DELETE_RELEASE';
+// export const DELETE_RELEASE_SUCCESS = 'DELETE_RELEASE_SUCCESS';
+// export const DELETE_RELEASE_ERROR = 'DELETE_RELEASE_ERROR';
+
+export const CLAER_RELEASE = 'CLAER_RELEASE';
+
 export const getReleases = createPromiseThunk(GET_RELEASES, api.getReleaseList);
 
 export const getRelease = createPromiseThunk(GET_RELEASE, api.getReleaseInfo);
@@ -27,3 +33,10 @@ export const postRelease = createPromiseThunk(
 );
 
 export const putRelease = createPromiseThunk(PUT_RELEASE, api.updateRelease);
+
+export const deleteRelease = createPromiseThunk(
+  DELETE_RELEASE,
+  api.deleteRelease,
+);
+
+export const clearRelease = () => ({ type: CLAER_RELEASE });
