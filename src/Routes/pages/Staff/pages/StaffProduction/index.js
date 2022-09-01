005329b2c96b6ba1df9d22b4a800322fd80b8614
parkhanju <parkhanju95@gmail.com>
2022-09-01 17:10:38 +0900

    feat: Update StaffProduction index
    update StaffProduction index

--- src/Routes/pages/Staff/pages/StaffProduction/index.js
+++ src/Routes/pages/Staff/pages/StaffProduction/index.js
@@ -1,3 +1,3 @@
-export { default as StaffProductionList } from 'routes/pages/Staff/pages/StaffProduction/ProductionList';
-export { default as StaffProductionRegistration } from 'routes/pages/Staff/pages/StaffProduction/ProductionRegistration';
-export { default as StaffProductionDetails } from 'routes/pages/Staff/pages/StaffProduction/ProductionDetails';
+export { default as StaffProductionList } from './ProductionList';
+export { default as StaffProductionRegistration } from './ProductionRegistration';
+export { default as StaffProductionDetails } from './ProductionDetails';
