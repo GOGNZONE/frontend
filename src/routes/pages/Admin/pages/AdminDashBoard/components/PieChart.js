dc1b2c648423cfdef7cc9c39079236e4e5129462
parkhanju <parkhanju95@gmail.com>
2022-09-13 18:11:46 +0900

    feat: Add PieChart component
    add PieChart component

--- src/routes/pages/Admin/pages/AdminDashBoard/components/PieChart.js
+++ src/routes/pages/Admin/pages/AdminDashBoard/components/PieChart.js
@@ -17,8 +17,11 @@ const PieChart = ({ releaseConfirmedList }) => {
     colorField: 'type',
     radius: 0.8,
     label: {
-      type: 'outer',
+      type: 'spider',
       content: '{value}개',
+      style: {
+        fontSize: 13,
+      },
     },
     interactions: [
       {
