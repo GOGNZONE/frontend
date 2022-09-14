4ef3a35c384524619bf827296e656613f5400d21
k-dhyeon <kdhyeon4237@gmail.com>
2022-09-14 14:38:15 +0900

    1438

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "> Build 파일 복사"
-cp -r /home/ubuntu/frontend/zip/build/* /home/ubuntu/file/
+mkdir /home/ubuntu/front-file
+cp -r /home/ubuntu/frontend/zip/build /home/ubuntu/file/
 sudo systemctl restart nginx
